c8217df91fe074b1f2e01a693e931efd0c5b9d95
PratikGade <102720462+pratikgade49@users.noreply.github.com>
2025-10-09 10:09:01 +0530

    Updated api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 // const API_BASE_URL = 'http://10.213.1.29:8000';
-export const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+export { API_BASE_URL };
 
 export interface UploadResponse {
   message: string;
